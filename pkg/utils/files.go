9a7385a9b3206f8b708603f5484456538765de8b
Oleh Astappiev <oleh@astappiev.me>
2023-09-19 22:35:35 +0200

    chore: merge actions

--- pkg/utils/files.go
+++ pkg/utils/files.go
@@ -62,7 +62,7 @@ func extensionByType(typ string) string {
 func DownloadAndPutObject(url string, bucket string, path string) (string, error) {
 	var err error
 	var resp *http.Response
-	if resp, err = http.Get(url); err != nil { //nolint
+	if resp, err = http.Get(url); err != nil { //#nosec G107
 		return "", err
 	}
 	defer resp.Body.Close()
